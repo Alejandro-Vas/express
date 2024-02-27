d4a25183cdf98f11671dc32ced23f628876e4f35
avasin <avasin@studio-tg.ru>
2024-02-27 22:34:49 +0300

    mongoose added

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+// eslint-disable-next-line import/extensions
+import Post from './Post.js';
 
 const PORT = 3000;
+const DB_URL = 'mongodb+srv://avasindev:hOkrHaRBuhEtxpTY@cluster0.ctdrist.mongodb.net/?retryWrites=true&w=majority';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json('123');
+app.post('/', async (req, res) => {
+  const {
+    author, title, content,
+  } = req.body;
+
+  const post = await Post.create({
+    author, title, content,
+  });
+
+  res.json(post);
 });
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running on port ${PORT}`);
-});
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (e) {
+    //
+  }
+}
+
+startApp();
